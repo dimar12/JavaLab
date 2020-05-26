989a522a835658b24c015411ef41129d0ccc3103
dimar12 <lyubovnikov2000@gmail.com>
2020-05-26 18:51:19 +0700

    add synchronized

--- src/com/company/myThread.java
+++ src/com/company/myThread.java
@@ -2,16 +2,19 @@ package com.company;
 
 public class myThread extends Thread {
     private final StorageForValue storage;
+
     public myThread(StorageForValue storage) {
         this.storage = storage;
     }
+
     @Override
     public void run() {
-        for (int i = 0; i < 10; ++i) {
-
+        for(int i = 0; i<10;i++) {
+            synchronized (storage) {
                 this.storage.value += 1;
                 System.out.println("[myThread] value = " + storage.value);
-
+            }
         }
     }
 }
+
--- src/com/company/myThread2.java
+++ src/com/company/myThread2.java
@@ -1,17 +1,19 @@
 package com.company;
 
-public class myThread2 extends Thread{
+public class myThread2 extends Thread {
     private final StorageForValue storage;
+
     public myThread2(StorageForValue storage) {
         this.storage = storage;
     }
+
     @Override
     public void run() {
-        for (int i = 0; i < 10; ++i) {
-
+        for(int i = 0; i<10;i++) {
+            synchronized (storage) {
                 this.storage.value -= 1;
                 System.out.println("[myThread2] value = " + storage.value);
-
+            }
         }
     }
 }
